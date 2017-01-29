79db4d5f63b5fade06de64486e72f320b6c6567b
Andre Ambrosio Boechat <boechat107@gmail.com>
2017-01-29 13:15:24 -0200

    Answer to exercise 1

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -4,15 +4,34 @@ package fpinscala.errorhandling
 import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = sys.error("todo")
+  // ======= My code starts here =====
+  // Exercise 1
+  def map[B](f: A => B): Option[B] =
+    this match {
+      case None => None
+      case Some(x) => Some(f(x))
+    }
 
-  def getOrElse[B>:A](default: => B): B = sys.error("todo")
+  def getOrElse[B>:A](default: => B): B =
+    this match {
+      case None => default
+      case Some(x) => x
+    }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = sys.error("todo")
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    this.map(f).getOrElse(None)
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = sys.error("todo")
+  def orElse[B>:A](ob: => Option[B]): Option[B] =
+    this.map(Some(_)).getOrElse(ob)
+    // this match {
+    //   case None => ob
+    //   case Some(x) => Some(x)
+    // }
 
-  def filter(f: A => Boolean): Option[A] = sys.error("todo")
+  def filter(f: A => Boolean): Option[A] =
+    if (this.map(f).getOrElse(false)) this
+    else None
+  // ===== End of my code ====
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
@@ -38,6 +57,8 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
+
+  // ======= My code starts here =====
   def variance(xs: Seq[Double]): Option[Double] = sys.error("todo")
 
   def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
