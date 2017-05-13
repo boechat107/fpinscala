77e464724cc7e264259aa39b59c7e71683acce8f
Andre Ambrosio Boechat <boechat107@gmail.com>
2017-05-13 08:50:17 -0300

    Exercise 8.4: Gen.choose

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -14,22 +14,37 @@ shell, which you can fill in and modify while working through the chapter.
 */
 
 trait Prop {
+  def check: Either[(FailedCase, SuccessCount), SuccessCount]
+  def &&(p: Prop): Prop
 }
 
 object Prop {
+  type FailedCase = String
+  type SuccessCount = Int
+
   def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
 }
 
 object Gen {
+  def choose(start: Int, stopExclusive: Int): Gen[Int] =
+    Gen(State (rng => {
+                 def recur (r: RNG): (Int, RNG) = {
+                   val (i, rng2) = r.nextInt
+                   if (i >= start && i < stopExclusive) (i, rng2)
+                   else recur(rng2)
+                 }
+                 recur (rng)}))
+
   def unit[A](a: => A): Gen[A] = ???
 }
 
-trait Gen[A] {
-  def map[A,B](f: A => B): Gen[B] = ???
-  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
-}
+case class Gen[A](sample: State[RNG,A])
+
+// trait Gen[A] {
+//   def map[A,B](f: A => B): Gen[B] = ???
+//   def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
+// }
 
 trait SGen[+A] {
 
 }
-
