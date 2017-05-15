e550c7c8042a41cb9f3851bd0a9ea3af5188a3c7
Andre Ambrosio Boechat <boechat107@gmail.com>
2017-05-15 09:52:18 -0300

    Exercise 8.5: unit, boolean and listOfN

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -35,7 +35,21 @@ object Gen {
                  }
                  recur (rng)}))
 
-  def unit[A](a: => A): Gen[A] = ???
+  def unit[A](a: => A): Gen[A] =
+    Gen(State(rng => (a, rng)))
+
+  def boolean: Gen[Boolean] =
+    Gen(State(rng => {
+                val (x, rng2) = rng.nextInt
+                if (x % 2 == 0) (true, rng2)
+                else (false, rng2)
+              }))
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] = {
+    val ls: List[State[RNG,A]] = (0 to n).toList map (i => g.sample)
+    Gen(State.sequence(ls))
+  }
+
 }
 
 case class Gen[A](sample: State[RNG,A])
